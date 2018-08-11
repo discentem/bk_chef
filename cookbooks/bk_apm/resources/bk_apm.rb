8642d41ead48f5179ec49180bca3b96f882a1a31
Brandon Kurtz <kurtz.brandon@gmail.com>
2018-08-10 23:48:25 -0400

    it's idempotent!!

--- cookbooks/bk_apm/resources/bk_apm.rb
+++ cookbooks/bk_apm/resources/bk_apm.rb
@@ -1,17 +1,21 @@
+require 'pry'
+
 resource_name :bk_apm
 default_action :run
 
 action :run do
 
   # return if node['bk_apm']['packages'].nil?
-  packages = ['platformio-ide-terminal']
 
   apm_cmd = node['bk_apm']['cmd']
-  packages.each do |package|
+  node['bk_apm']['packages'].each do |package|
+
+    apm_list = "& \"#{apm_cmd}\" list | select-string #{package}"
     execute "install apm package: #{package}" do
       command "\"#{apm_cmd}\" install #{package}"
-      not_if powershell_out("\'#{apm_cmd}\' list) | select-string #{package}").stdout.include?(package)
+      not_if { powershell_out(apm_list).stdout.include?(package) }
       action :run
     end
+    
   end
 end
